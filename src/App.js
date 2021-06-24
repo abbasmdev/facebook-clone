4e82d2defab4c563202062453f302992f4bbc7a3
Abbas Moharami <abbasmdev@gmail.com>
2021-06-24 11:10:52 +0430

    1. Added react-router-dom package for routing. 2. Added Home, Auth pages. 3, Configured routing for Auth, Home pages.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import AuthPage from "./pages/Auth";
+import HomePage from "./pages/Home";
+
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
-      <h1>Build Facebook clone</h1>
+      <Switch>
+        <Route path="/auth">
+          <AuthPage />
+        </Route>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
