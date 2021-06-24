52bb5531f0a6849106030075a9fdbe6ded5b8896
Abbas Moharami <abbasmdev@gmail.com>
2021-06-24 17:45:45 +0430

    Added updating auth state slice with changing state of firebase auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { Switch, Route, Redirect, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { auth as firebaseAuth } from "./firebase/firebase";
+import { authActions } from "./store/auth/authSlice";
 import AuthPage from "./pages/Auth";
 import HomePage from "./pages/Home";
 
 import styles from "./App.module.css";
-import { auth as firebaseAuth } from "./firebase/firebase";
 
 function App() {
+  const dispatch = useDispatch();
   const [authUser, authLoading, authError] = useAuthState(firebaseAuth);
   const history = useHistory();
   firebaseAuth.onAuthStateChanged((user) => {
     if (user) {
+      dispatch(authActions.setUser(user));
       history.replace("/");
     } else {
+      dispatch(authActions.clearUser());
       history.replace("/auth");
     }
   });
