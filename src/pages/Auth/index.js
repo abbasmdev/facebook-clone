1beb321474b9921d380a898163431a546aa4c546
Abbas Moharami <abbasmdev@gmail.com>
2021-06-24 17:07:29 +0430

    Completed auth page.

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,5 +1,23 @@
+import { Button } from "@material-ui/core";
+import styles from "./index.module.css";
 function Auth() {
-  return <div>Auth page</div>;
+  const loginBtnClickHandler = () => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginContainer}>
+        <img
+          src="/images/logo.png"
+          alt="Facebook logo"
+          width="100"
+          height="100"
+        />
+        <Button onClick={loginBtnClickHandler} variant="outlined">
+          Login with google
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
